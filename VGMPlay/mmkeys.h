71ff02e2198c9253bd8d996d31a2e0b52e15a470
Tasos Sahanidis <tasos@tasossah.com>
2018-03-06 18:55:45 +0200

    Add extra metadata and missing signal definition

--- VGMPlay/mmkeys.h
+++ VGMPlay/mmkeys.h
@@ -12,6 +12,8 @@ typedef void (*mmkey_cbfunc)(UINT8 event);
 #define SIGNAL_PLAYSTATUS  0x02 // Playback Status Changed
 #define SIGNAL_SEEKSTATUS  0x04 // Seek Status Changed
 #define SIGNAL_SEEK        0x08 // Seeked
+#define SIGNAL_CONTROLS    0x10 // Playback controls need to be updated (CanGoNext/Previous)
+#define SIGNAL_ALL         0xFF // All Signals
 
 UINT8 MultimediaKeyHook_Init(void);
 void MultimediaKeyHook_Deinit(void);
